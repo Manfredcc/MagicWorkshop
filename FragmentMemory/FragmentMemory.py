eadf925e1173ff8be56ea49194f3f76f0ba25851
Manfred <1259106665@qq.com>
2024-12-01 21:57:59 +0800

    [Project]: Fragment Memory
    [Descrip]: Add configuration file : defConf.json
    
    [Details]: Modify defConf.json to change UI or just selected another
    configuration file in loadConf()
    
    [Comment]: Harder work leads to a greter feeling of accomplishment

--- FragmentMemory/FragmentMemory.py
+++ FragmentMemory/FragmentMemory.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import json
+from datetime import datetime
 import os
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QLabel, QStackedWidget, QSizePolicy, QSpacerItem, QLineEdit
 from PyQt5.QtCore import QThread, pyqtSignal, QRect, Qt, QSize
@@ -11,15 +13,15 @@ class Tag(QWidget):
         self.parent = parent
         layout = QVBoxLayout(self)
         self.label = QLabel(f'{tag} hello world')
-        self.label.setFont(QFont(self.parent.basicFont, 10))
+        self.label.setFont(QFont("Microsoft YaHei", 10))
         self.label3 = QLabel(f'{tag} I am constanc while world changes')
-        self.label3.setFont(QFont(self.parent.basicFont, 10))
+        self.label3.setFont(QFont("Microsoft YaHei", 10))
         self.label4 = QLabel(f'{tag} I love u, pidundun')
-        self.label4.setFont(QFont(self.parent.basicFont, 10))
+        self.label4.setFont(QFont("Microsoft YaHei", 10))
         self.label5 = QLabel(f'{tag} I miss u, a pi')
-        self.label5.setFont(QFont(self.parent.basicFont, 10))
+        self.label5.setFont(QFont("Microsoft YaHei", 10))
         self.label6 = QLabel(f'{tag} no matter where you are')
-        self.label6.setFont(QFont(self.parent.basicFont, 10))
+        self.label6.setFont(QFont("Microsoft YaHei", 10))
         layout.addWidget(self.label)
         layout.addWidget(self.label3)
         layout.addWidget(self.label4)
@@ -31,142 +33,103 @@ class App(QWidget):
         super().__init__()
         self.initUI()
         self.initData()
-        pass
 
     def initUI(self):
-        self.mainLayout()
-        self.loadColor()
-        self.loadWelcome()
-        self.loadSearch()
-        self.loadTag()
-        self.basciOperation()
-        self.bind()
-    
-    def initData(self):
-        pass
+        self.loadConf() # Keep this first
+        self.initLayout()
+        # self.bind()
 
-    # 主界面
-    def mainLayout(self):
-        # 主界面基础设置
-        self.basicFont = 'Microsoft YaHei'
-        self.setGeometry(300, 300, 1200, 650)
-        # self.setWindowTitle('Fragment Memory')
-        self.setWindowTitle('记忆碎片')
+    def loadConf(self):
+        self.curPath = os.path.dirname(os.path.abspath(__file__))
+        self.confFile = r'\conf\defCOnf.json'
+        with open(self.curPath + self.confFile, 'r', encoding='utf-8') as file:
+            self.confData = json.load(file)
 
-    # 加载颜色主题
-    def loadColor(self): # todo
-        pass
+    def layoutConfigure(self, obj, layout, index=-1):
+        # 检查并设置位置大小
+        if set({'x', 'y', 'w', 'h'}).issubset(layout.keys()):
+            if -1 == index:
+                obj.setGeometry(QRect(layout['x'], layout['y'], layout['w'], layout['h']))
+            elif 'xOffset' in layout:
+                obj.setGeometry(QRect(layout['x'] + index * layout['xOffset'], layout['y'], layout['w'], layout['h']))
+            elif 'yOffset' in layout:
+                obj.setGeometry(QRect(layout['x'], layout['y'] + index * layout['yOffset'], layout['w'], layout['h']))
+            else:
+                print('invalid parameter')
+                
+        # 检查并设置字体
+        if set({'font', 'fontsize'}).issubset(layout.keys()):
+            qfont = QFont(layout['font'], layout['fontsize'])
+            if 'bold' in layout and layout['bold'] == True:
+                qfont.setBold(True)
+            obj.setFont(qfont)
+        # 检查并设置基本样式
+        if 'style' in layout:
+            obj.setStyleSheet(layout['style'])
 
-    def loadWelcome(self):
-        self.welcomeLabel = QLabel('夜深了，晚上好', self) # fix me! According the time
-
-        # self.welcomeLabel = QLabel(self.welcome)
-        self.welcome = ''
-        self.welcomeLabel.setGeometry(QRect(105, 20, 400, 35)) # 使用绝对位置，保持左上角
-        welcomeFont = QFont(self.basicFont, 14)
-        welcomeFont.setBold(True)
-        self.welcomeLabel.setFont(welcomeFont)
-
-    # 加载搜索
-    def loadSearch(self):
+    def initLayout(self):
+        # 主界面
+        jdata = self.confData['main']
+        self.setWindowTitle(jdata['title'])
+        self.layoutConfigure(self, jdata['layout'])
+        # 欢迎词
+        self.welcomeLabel = QLabel(self.getWelcome(), self)
+        self.layoutConfigure(self.welcomeLabel, self.confData['welcome']['layout'])
+        # 搜索框
         self.searchBox = QLineEdit(self)
-
-        self.searchBox.setPlaceholderText('输入搜索内容')
-        self.searchBox.setFont(QFont(self.basicFont, 15))
-        self.searchBox.setStyleSheet("QLineEdit:placeholder { color: gray; }")
-        self.searchBox.setStyleSheet("QLineEdit { border: 2px solid gray; }")
-        self.searchBox.setGeometry(QRect(80, 90, 650, 75))
-
-    # 加载标签
-    def loadTag(self):
-        self.tagList = ['最近', '收藏', '项目'] # fix me! parse .xml
-        self.tagButtonList = []
-        self.stackedWidget = QStackedWidget(self)
-        self.stackedWidget.setGeometry(QRect(70, 260, 650, 400))
+        self.searchBox.setPlaceholderText(self.confData['search']['title'])
+        self.layoutConfigure(self.searchBox, self.confData['search']['layout'])
+        # 基本标签页面
+        self.page = QStackedWidget(self)
+        self.layoutConfigure(self.page, self.confData['page']['layout'])
+        # 标签按钮
+        self.tagButtonList = [] # 保存所有的标签按钮实例
+        self.tagNameList = list(self.confData['tagButton']['name'].values())
         i = 0
-        self.defButtonStyle = """
-            QPushButton {
-                background-color: transparent; /* 背景透明 */
-                border: none; /* 移除所有边框 */
-                padding: 5px; /* 内边距，根据需要调整 */
-            }
-            QPushButton:pressed {
-                background-color: #d0d0d0; /* 点击时的背景色，可选 */
-            }
-        """
-        self.pushButtonStyle = """
-            QPushButton {
-                background-color: transparent; /* 背景透明 */
-                border: none; /* 移除所有边框 */
-                border-bottom: 2px solid gray; /* 添加下边框 */
-                padding: 5px; /* 内边距，根据需要调整 */
-            }
-        """
-        for t in self.tagList: # 创建页面并绑定指定按钮
-            self.stackedWidget.addWidget(Tag(t, self))
+        for t in self.tagNameList: # 为每个按钮创建页面内容，并添加到page中统一管理
+            self.page.addWidget(Tag(t, self))
             button = QPushButton(t, self)
-            button.setFont(QFont(self.basicFont, 12))
+            self.layoutConfigure(button, self.confData['tagButton']['layout'], i)
             button.clicked.connect(self.tagButtonCallback)
-            button.setGeometry(QRect(80 + (100 * i), 210, 60, 40))
-            button.setStyleSheet(self.defButtonStyle)
             self.tagButtonList.append(button)
             i += 1
-        # 默认第一个被按下
-        self.tagButtonList[0].setStyleSheet(self.pushButtonStyle)
+        self.tagButtonList[0].setStyleSheet(self.confData['tagButton']['layout']['selectedStyle']) # 默认选中第一个页面，更改按钮样式
+        # 基本操作图标
+        self.opsButtonList = []
+        i = 0
+        for path in list(self.confData['opsButton']['path'].values()):
+            button = QPushButton(self)
+            button.setIcon(QIcon(self.curPath + path))
+            button.setIconSize(QSize(self.confData['opsButton']['iconSize'], self.confData['opsButton']['iconSize']))
+            self.layoutConfigure(button, self.confData['opsButton']['layout'], i)
+            self.opsButtonList.append(button)
+            i += 1
 
-    def tagButtonCallback(self):
-        # 根据按下的不同按钮，确定切换到哪个页面
+    def getWelcome(self):
+        hour = datetime.now().hour
+        if 0 <= hour < 5:       return self.confData['welcome']['greeting']['midnight']
+        elif 5 <= hour < 11:    return self.confData['welcome']['greeting']['morning']
+        elif 1 <= hour < 13:    return self.confData['welcome']['greeting']['noon']
+        elif 13 <= hour < 18:   return self.confData['welcome']['greeting']['afternoon']
+        elif 18 <= hour < 22:   return self.confData['welcome']['greeting']['evening']
+        else:                   return self.confData['welcome']['greeting']['night']
+
+    def tagButtonCallback(self): # 根据按下的不同按钮，切换到指定页面
         clickedButton = self.sender()
-        buttonName = clickedButton.text()
-        print(f"按键 {buttonName} 被按下")
-        index = self.tagList.index(buttonName)
-        self.stackedWidget.setCurrentIndex(index)
-        # 改变按钮颜色
-        for b in self.tagButtonList:
+        index = self.tagNameList.index(clickedButton.text())
+        self.page.setCurrentIndex(index)
+        for b in self.tagButtonList: # 颜色标记选中的页面对应的按钮
             if b == clickedButton:
-                b.setStyleSheet(self.pushButtonStyle)
+                b.setStyleSheet(self.confData['tagButton']['layout']['selectedStyle'])
             else:
-                b.setStyleSheet(self.defButtonStyle)
-
-    # 创建基本操作图标
-    def basciOperation(self):
-        urlFather = os.path.dirname(os.path.abspath(__file__))
-        self.operationButtonStyle = """
-            QPushButton {
-                background-color: transparent; /* 背景透明 */
-                border: none; /* 移除所有边框 */
-                padding: 5px; /* 内边距，根据需要调整 */
-            }
-            QPushButton:pressed {
-                background-color: #d0d0d0; /* 点击时的背景色，可选 */
-            }
-        """
-        # 新建
-        path = urlFather + (r'\icon\new.png')
-        self.newButton = QPushButton(self)
-        self.newButton.setStyleSheet(self.operationButtonStyle)
-        self.newButton.setIcon(QIcon(path))
-        self.newButton.setIconSize(QSize(150, 150))
-        self.newButton.setGeometry(QRect(990, 40, 150, 150))
-        # 浏览文件
-        path = urlFather + (r'\icon\open.png')
-        self.openButton = QPushButton(self)
-        self.openButton.setStyleSheet(self.operationButtonStyle)
-        self.openButton.setIcon(QIcon(path))
-        self.openButton.setIconSize(QSize(125, 125))
-        self.openButton.setGeometry(QRect(1000, 240, 125, 125))
-        # 导入他人分享的文件
-        path = urlFather + (r'\icon\import.png')
-        self.importButton = QPushButton(self)
-        self.importButton.setStyleSheet(self.operationButtonStyle)
-        self.importButton.setIcon(QIcon(path))
-        self.importButton.setIconSize(QSize(125, 125))
-        self.importButton.setGeometry(QRect(1000, 440, 125, 125))
+                b.setStyleSheet(self.confData['tagButton']['layout']['style'])
 
     # 绑定UI和操作
     def bind(self):
         pass
 
+    def initData(self):
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
